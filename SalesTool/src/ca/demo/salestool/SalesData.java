0d69ead8fde0d955a68b91e2385ebbf1d1882406
rodolfowei <crm_b@hotmail.com>
2015-05-12 15:31:32 +0200

    Completed displaying data

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -7,17 +7,18 @@ public class SalesData {
 	int data[] = {-1, 0, 4, 42, 84};
 	
 	public void display() {
+		
 		System.out.println("Data: ");
 
 		int sum = 0;
-		for (int i: data) {
+		for (int i=0; i<data.length;i++) {
 			System.out.println("Next value: " + data[i]);
 			sum += i;
 		}
 		
 		//new for
-		for (int j: data){
-			System.out.println(j);
+		for (int j=0; j<data.length;j++) {
+			System.out.println("Next value:" + j);
 		}
 		
 		System.out.println("Sum is: " + sum);
